e06bb69e88d9f46ca5160bcba26ee23a101c5972
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-09 14:38:12 +0200

    Initial commit

---
+++ src/main/java/fon/bg/ac/rs/schooloflanguages/SchoolOfLanguagesApplication.java
@@ -0,0 +1,13 @@
+package fon.bg.ac.rs.schooloflanguages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolOfLanguagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolOfLanguagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fon/bg/ac/rs/schooloflanguages/SchoolOfLanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package fon.bg.ac.rs.schooloflanguages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolOfLanguagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
