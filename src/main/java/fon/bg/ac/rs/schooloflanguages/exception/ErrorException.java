82c68d420c78156fd23d76200566d7a1dca080bc
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-11 14:29:35 +0200

    Kreirani tipovi Exception-a

---
+++ src/main/java/fon/bg/ac/rs/schooloflanguages/exception/ErrorException.java
@@ -0,0 +1,11 @@
+package fon.bg.ac.rs.schooloflanguages.exception;
+
+public class ErrorException extends ApplicationException{
+
+	public ErrorException(Object entity, String message) {
+		super(message);
+	}
+	
+	
+
+}
