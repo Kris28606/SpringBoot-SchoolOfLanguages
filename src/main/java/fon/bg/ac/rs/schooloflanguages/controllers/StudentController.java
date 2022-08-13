bbbdb2d208163d415f0eef075027a5936b912bd9
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-13 23:27:13 +0200

    Implementirana ruta get/all za studente

--- src/main/java/fon/bg/ac/rs/schooloflanguages/controllers/StudentController.java
+++ src/main/java/fon/bg/ac/rs/schooloflanguages/controllers/StudentController.java
@@ -1,9 +1,13 @@
 package fon.bg.ac.rs.schooloflanguages.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fon.bg.ac.rs.schooloflanguages.dto.StudentDto;
 import fon.bg.ac.rs.schooloflanguages.mapper.StudentMapper;
 import fon.bg.ac.rs.schooloflanguages.service.StudentService;
 
@@ -16,5 +20,10 @@ public class StudentController {
 	
 	public StudentController() {
 		studentMapper=new StudentMapper();
+	}
+	
+	@GetMapping("all")
+	public List<StudentDto> vratiSve() {
+		return studentService.getAll();
 	}
 }
--- src/main/java/fon/bg/ac/rs/schooloflanguages/service/StudentService.java
+++ src/main/java/fon/bg/ac/rs/schooloflanguages/service/StudentService.java
@@ -1,9 +1,15 @@
 package fon.bg.ac.rs.schooloflanguages.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fon.bg.ac.rs.schooloflanguages.dto.StudentDto;
 import fon.bg.ac.rs.schooloflanguages.mapper.StudentMapper;
+import fon.bg.ac.rs.schooloflanguages.model.Student;
 import fon.bg.ac.rs.schooloflanguages.repository.StudentRepository;
 
 @Service
@@ -14,5 +20,16 @@ public class StudentService {
 	
 	public StudentService() {
 		studentMapper=new StudentMapper();
+	}
+
+	public List<StudentDto> getAll() {
+		List<Student> studenti=studentRepository.findAll();
+		if(studenti.isEmpty()) {
+			List<StudentDto> prazna=new ArrayList<>();
+			return prazna;
+		}
+		return studenti.stream().map((student)-> {
+			return studentMapper.toDto(student);
+		}).collect(Collectors.toList());
 	}
 }
