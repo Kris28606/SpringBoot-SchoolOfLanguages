eacee01dfcd9da5fdd37557d1311b6cbc9d277f4
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-13 23:26:24 +0200

    Dodato racunanje godina

--- src/main/java/fon/bg/ac/rs/schooloflanguages/mapper/StudentMapper.java
+++ src/main/java/fon/bg/ac/rs/schooloflanguages/mapper/StudentMapper.java
@@ -1,5 +1,11 @@
 package fon.bg.ac.rs.schooloflanguages.mapper;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Date;
+
+import org.springframework.format.datetime.standard.InstantFormatter;
+
 import fon.bg.ac.rs.schooloflanguages.dto.StudentDto;
 import fon.bg.ac.rs.schooloflanguages.model.Student;
 
@@ -11,8 +17,8 @@ public class StudentMapper implements GenericMapper<StudentDto, Student>{
 		s.setId(dto.getId());
 		s.setFirstName(dto.getFirstName());
 		s.setLastName(dto.getLastName());
-		s.setDatumRodjenja(dto.getDatumRodjenja());
 		s.setCourses(dto.getCourses());
+		s.setDatumRodjenja(dto.getDatumRodjenja());
 		return s;
 	}
 
@@ -24,6 +30,16 @@ public class StudentMapper implements GenericMapper<StudentDto, Student>{
 		s.setLastName(e.getLastName());
 		s.setDatumRodjenja(e.getDatumRodjenja());
 		s.setCourses(e.getCourses());
+		long trenutnaGodina=Timestamp.from(Instant.now()).getYear();
+		long godinaRodjenja=e.getDatumRodjenja().getYear();
+		long mesecRodjenja=e.getDatumRodjenja().getMonth();
+		long danRodjenja=e.getDatumRodjenja().getDate();
+		long trenutniMesec=Timestamp.from(Instant.now()).getMonth();
+		long trenutniDan=Timestamp.from(Instant.now()).getDate();
+		s.setYears((int) (trenutnaGodina-godinaRodjenja));
+		if(trenutniMesec<=mesecRodjenja && trenutniDan<danRodjenja) {
+			s.setYears(s.getYears()-1);
+		}
 		return s;
 	}
 
