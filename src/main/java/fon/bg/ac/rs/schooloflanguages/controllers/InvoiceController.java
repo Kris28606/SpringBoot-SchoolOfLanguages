82719ef990d2fabe7cfe1bce71f8ff1fb173cb0f
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-16 00:48:55 +0200

    Dodata ruta za storniranje fakture

--- src/main/java/fon/bg/ac/rs/schooloflanguages/controllers/InvoiceController.java
+++ src/main/java/fon/bg/ac/rs/schooloflanguages/controllers/InvoiceController.java
@@ -3,12 +3,18 @@ package fon.bg.ac.rs.schooloflanguages.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fon.bg.ac.rs.schooloflanguages.dto.InvoiceDto;
+import fon.bg.ac.rs.schooloflanguages.exception.ErrorException;
 import fon.bg.ac.rs.schooloflanguages.service.InvoiceService;
 
 @RestController
@@ -22,5 +28,17 @@ public class InvoiceController {
 	public List<InvoiceDto> vratiSve() {
 		return invoiceService.getAll();
 	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<Object> stornirajFakturu(@PathVariable("id") Long id){
+		try {
+			return ResponseEntity.ok(invoiceService.storniraj(id));
+		} catch(ErrorException ex) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+	
 
 }
--- src/main/java/fon/bg/ac/rs/schooloflanguages/service/InvoiceService.java
+++ src/main/java/fon/bg/ac/rs/schooloflanguages/service/InvoiceService.java
@@ -1,12 +1,14 @@
 package fon.bg.ac.rs.schooloflanguages.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fon.bg.ac.rs.schooloflanguages.dto.InvoiceDto;
+import fon.bg.ac.rs.schooloflanguages.exception.ErrorException;
 import fon.bg.ac.rs.schooloflanguages.mapper.InvoiceMapper;
 import fon.bg.ac.rs.schooloflanguages.model.Invoice;
 import fon.bg.ac.rs.schooloflanguages.repository.InvoiceRepository;
@@ -26,5 +28,19 @@ public class InvoiceService {
 		return invoices.stream().map((invoice)-> {
 			return invoiceMapper.toDto(invoice);
 		}).collect(Collectors.toList());
+	}
+
+	public InvoiceDto storniraj(Long id) throws ErrorException {
+		Optional<Invoice> optional=invoiceRepository.findById(id);
+		if(!optional.isPresent()) {
+			throw new ErrorException("Invoice doesn't exist!");
+		}
+		Invoice i=optional.get();
+		if(i.isCancelled()) {
+			throw new ErrorException("Invoice is allredy reverted!");
+		}
+		i.setCancelled(true);
+		i= invoiceRepository.save(i);
+		return invoiceMapper.toDto(i);
 	}
 }
