1c318abb47cc18af6f95c3e5ed01422346a05eba
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-01 15:06:47 +0200

    Dokumentovani Exception-i

--- src/main/java/fon/bg/ac/rs/schooloflanguages/exception/ErrorException.java
+++ src/main/java/fon/bg/ac/rs/schooloflanguages/exception/ErrorException.java
@@ -1,7 +1,16 @@
 package fon.bg.ac.rs.schooloflanguages.exception;
 
+/**
+ * Korisnicki definisan exception koji nasledjuju ApplicationException
+ * @author Kristina
+ *
+ */
 public class ErrorException extends ApplicationException{
 
+	/**
+	 * Konstruktor koji prihvata poruku
+	 * @param message - Poruka za exception
+	 */
 	public ErrorException(String message) {
 		super(message);
 	}
